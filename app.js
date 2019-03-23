eba6ecfa86f69a69770347ea230cb01a4b8db6d5
DevMex <cmanuel.tperaza@gmail.com>
2019-03-22 21:06:13 -0600

    Add MongoDB connection

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 const port = require('./config')
+const mongoose = require('mongoose')
+const app = express()
+
+
+mongoose.connect(configdatabase_url, { useNewUrlParser:true})
+    .then(
+        () => {
+            console.log("Funciona")
+        },
+        err => {
+            console.error(err)
+          }
+    )
+
 
 app.get('/', (req, res) => res.json({"Text":'Hello World!', "Version":"1"}))
 
